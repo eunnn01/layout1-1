bbb1cc00549d49b6ff441ce84ad7f5c5872ff71c
eunnn <eunjae5180@gmail.com>
2023-11-30 09:42:31 +0900

    v1.0

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function(){
+    console.log("load");
+    $(".main-nav").mouseenter(function(){
+        $(".drop").stop().slideDown();
+    })
+    $(".main-nav").mouseleave(function(){
+        $(".drop").stop().slideUp();
+    })
+
+    
+})
